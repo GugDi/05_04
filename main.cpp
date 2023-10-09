a6f4a09091bc693de647c9e54c3dcdfa8c9d5dc4
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 19:31:56 +0400

    main.cpp - Вывод в CLS 2 книги (Название, Автор, Год)

--- main.cpp
+++ main.cpp
@@ -6,4 +6,5 @@ int main() {
   TBook book2("To Kill a Mockingbird", "Harper Lee", 1960);
 
   printf("%s by %s (%d)\n", book1.getTitle().c_str(), book1.getAuthor().c_str(), book1.getYear());
+  printf("%s by %s (%d)\n", book2.getTitle().c_str(), book2.getAuthor().c_str(), book2.getYear());
 };
