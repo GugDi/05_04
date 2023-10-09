237420bf1fc264339906432a321e5fb7ee8187cc
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 19:22:30 +0400

    TBook.h - публичные переменные класса TBook

--- TBook.h
+++ TBook.h
@@ -3,10 +3,17 @@
 
 #include <string>
 class TBook {
-private:
-    std::string m_title;
-    std::string m_author;
-    int m_year;
+    public:
+        TBook();
+        TBook(std::string title, std::string author, int year);
+        std::string getTitle();
+        std::string getAuthor();
+        int getYear();
+
+    private:
+        std::string m_title;
+        std::string m_author;
+        int m_year;
 };
 
 #endif
