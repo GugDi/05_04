7a78c32bef947b3bd65ed04c869a527ab01a4b7a
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 19:11:50 +0400

    Минимальная компиляция программы

--- TBook.h
+++ TBook.h
@@ -1,20 +1,3 @@
 #ifndef TBOOK_H
 #define TBOOK_H
-
-#include <string>
-
-class TBook {
-public:
-    TBook();
-    TBook(std::string title, std::string author, int year);
-    std::string getTitle();
-    std::string getAuthor();
-    int getYear();
-
-private:
-    std::string m_title;
-    std::string m_author;
-    int m_year;
-};
-
 #endif
