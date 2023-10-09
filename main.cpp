0120f4c85bc50ba73d144af166d6ace923b1e0a3
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 19:11:01 +0400

    Создание файлов

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "TBook.h"
+
+int main() {
+    TBook book1("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+    TBook book2("To Kill a Mockingbird", "Harper Lee", 1960);
+
+    printf("%s by %s (%d)\n", book1.getTitle().c_str(), book1.getAuthor().c_str(), book1.getYear());
+    printf("%s by %s (%d)\n", book2.getTitle().c_str(), book2.getAuthor().c_str(), book2.getYear());
+
+    return 0;
+}
